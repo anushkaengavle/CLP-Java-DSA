9fa0493c9293af64cd90b9f0db46bc58d973bec7
Anushka Engavle <83860689+anushkaengavle@users.noreply.github.com>
2021-11-03 00:44:30 +0530

    feat:countDigit

--- Getting Started/CountDigits.java
+++ Getting Started/CountDigits.java
@@ -8,6 +8,19 @@ import java.util.*;
   public class CountDigits{
   
   public static void main(String[] args) {
-    // write your code here  
-   }
+    
+    Scanner sc = new Scanner(System.in);
+
+    int num,temp,count=0,rem=0;
+    System.out.print("Enter the Number:");
+    num = sc.nextInt();
+    temp=num;
+    while(temp!=0)
+    {
+      rem=temp%10;
+      temp/=10;
+      count+=1;
+    }
+    System.out.println("the number of digit in " +num+" are "+count);
+  }
   }
