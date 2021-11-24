0334e743c852a21f2e49ea01b5c545aae3d45f76
Anushka Engavle <83860689+anushkaengavle@users.noreply.github.com>
2021-11-25 02:26:53 +0530

    feat:code 1

---
+++ Patterns/Pattern1.java
@@ -0,0 +1,26 @@
+/**
+I/P-> 5
+O/P-> 
+*
+**
+***
+****
+*****     
+*/
+
+import java.util.*;
+
+public class Pattern1{
+  public static void main(String[] args) {
+      
+  } {
+    Scanner scn = new Scanner(System.in);
+    int n = scn.nextInt();
+    for (int i = 1; i <= n; i++) {
+        for(int j = 0; j<i;j++){
+            System.out.println("*");
+        }
+            
+    }
+  }
+}
